125050856979d101d4324ad9bdad8b0aa503360a
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-06-30 13:43:57 +0530

    Added BookRepoTest Comments : Documentation

--- src/test/java/com/dockerators/bookapp/Repository/BookRepoTest.java
+++ src/test/java/com/dockerators/bookapp/Repository/BookRepoTest.java
@@ -1,6 +1,5 @@
 package com.dockerators.bookapp.Repository;
 
-
 import com.dockerators.bookapp.dao.BookRepository;
 import com.dockerators.bookapp.entity.Book;
 import org.junit.jupiter.api.*;
@@ -20,64 +19,81 @@ public class BookRepoTest {
     @Autowired
     private BookRepository bookRepository;
 
+    // Test to save a book
     @Test
     @Order(1)
     @Rollback(value = false)
     public void saveBookTest(){
+        // Creating a book object
         Book book = new Book(1,"Study in Scarlet","Conan Doyle","Mystery Book","123456789");
         bookRepository.save(book);
+        // Verifying if the book object has been assigned an ID
         Assertions.assertTrue(book.getId() > 0);
     }
 
+    // Test to retrieve a book by ID
     @Test
     @Order(2)
     @AutoConfigureTestDatabase
     public void getBookTest(){
+        // Retrieving a book with ID 1
         Book search_book = bookRepository.findById(1).get();
-        Assertions.assertEquals(1, (search_book.getId()));
+        // Verifying if the retrieved book has the expected ID
+        Assertions.assertEquals(1, search_book.getId());
     }
 
+    // Test to check if a non-existent book is not present
     @Test
     @Order(3)
     @AutoConfigureTestDatabase
     public void getNonPresentBookTest(){
+        // Checking if a book with ID 2 is present
         boolean isPresent = bookRepository.findById(2).isPresent();
+        // Verifying that the book with ID 2 is not present
         Assertions.assertFalse(isPresent);
     }
 
+    // Test to retrieve a list of books
     @Test
     @Order(4)
     @AutoConfigureTestDatabase
     public void getListOfBooksTest(){
+        // Retrieving all books
         List<Book> books = bookRepository.findAll();
-        Assertions.assertTrue((books.size()) > 0);
+        // Verifying if the list of books is not empty
+        Assertions.assertTrue(books.size() > 0);
     }
 
+    // Test to update a book's title
     @Test
     @Order(5)
     @Rollback(value = false)
     @AutoConfigureTestDatabase
     public void updateBookTest(){
+        // Retrieving a book with ID 1
         Book book = bookRepository.findById(1).get();
-        book.setTitle("Hounds of baskervilles");
+        // Updating the book's title
+        book.setTitle("Hounds of Baskervilles");
         bookRepository.save(book);
-        Assertions.assertTrue(((bookRepository.findById(1).get()).getTitle()).equals("Hounds of baskervilles"));
+        // Verifying if the book's title has been updated
+        Assertions.assertTrue(bookRepository.findById(1).get().getTitle().equals("Hounds of Baskervilles"));
     }
 
-
+    // Test to delete a book
     @Test
     @Order(6)
     @AutoConfigureTestDatabase
     public void deleteBookTest(){
+        // Retrieving a book with ID 1
         Book book = bookRepository.findById(1).get();
+        // Deleting the book
         bookRepository.delete(book);
-        Book book1 = null;
-        Optional<Book> optionalBook = bookRepository.findByTitle("Hounds of baskervilles");
+        // Verifying if the book has been deleted
+        Book deletedBook = null;
+        Optional<Book> optionalBook = bookRepository.findByTitle("Hounds of Baskervilles");
         if(optionalBook.isPresent()){
-            book1 = optionalBook.get();
+            deletedBook = optionalBook.get();
         }
-        Assertions.assertTrue(book1 == null);
+        Assertions.assertTrue(deletedBook == null);
     }
-
 }
-
